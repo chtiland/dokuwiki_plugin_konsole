07d4f4b5300d611e8ab4447be15f39388a7b0438
chtiland <fabrice@chtiland.com>
2016-01-30 11:37:51 +0100

    Update action.php

--- action.php
+++ action.php
@@ -14,7 +14,7 @@ require_once(DOKU_PLUGIN.'action.php');
 class action_plugin_konsole extends DokuWiki_Action_Plugin {
 
 
-    function register(&$controller){
+    function register(Doku_Event_Handler $controller){
         $controller->register_hook('TOOLBAR_DEFINE', 'AFTER', $this, 'handle_toolbar', array ());
     }
 
